3564e6d206419359d01aca36934a144e96bac698
Alexey Salnikov <cartrege@gmail.com>
2014-11-02 18:42:50 +0300

    regions added

--- src/Regions.php
+++ src/Regions.php
@@ -10,4 +10,22 @@ class Regions
 {
     const MOSCOW = 1;
     const SPB = 2;
+    const VORONEZH = 3;
+    const BELGOROD = 4;
+    const KURSK = 5;
+    const TAMBOV = 6;
+    const LIPETSK = 7;
+    const OREL = 8;
+    const BRYANSK = 9;
+    const RYAZAN = 10;
+    const TULA = 11;
+    const KALUGA = 12;
+    const SMOLENSK = 13;
+    const VLADIMIR = 14;
+    const TVER = 15;
+    const IVANOVO = 16;
+    const YAROSLAVL = 17;
+    const KOSTROMA = 18;
+    const ROSTOV = 19;
+    const ROSTOV_ON_DON = 20;
 }
